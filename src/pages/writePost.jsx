b0fc1a8865997ab4b77ca610e8d2fd36f3cae6f8
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-02-04 21:45:31 +0800

    create write post

--- src/pages/writePost.jsx
+++ src/pages/writePost.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Separator } from "@/components/ui/separator";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const WritePost = () => {
-  return <div>WritePost</div>;
+  return <div className="w-screen h-screen px-9 py-16 flex flex-col gap-6 items-center overflow-x-hidden">
+    <img
+        className=" w-full h-full object-cover fixed -z-10 brightness-[0.15]"
+        src="/images/star-bg.png"
+        alt="cube background image"
+      />
+    <div className="flex flex-col gap-4 w-full">
+      <h1 className=" text-5xl">Write a post</h1>
+      <p className="text-muted-foreground">Share your tech-tacular post that peaks the community`s interest</p>
+      <Separator/>
+    </div>
+    <form className="w-full">
+      <div className="flex flex-col gap-8 items-center px-[20%] w-full ">
+        <div className="flex flex-col gap-3 w-full">
+          <Label htmlFor="title">Title</Label>
+          <Input className="bg-black" id="title" placeholder="Bro Richie Finally Created GPT 5.0" />
+          <p className="text-sm text-muted-foreground">
+            Write an eye-catching title that turns eyeballs into clickbait magnets.
+          </p>
+        </div>
+        <div className="flex flex-col gap-3 w-full">
+          <Label htmlFor="tags">Tags</Label>
+          <Input className="bg-black" id="tags" placeholder="shadcn" />
+          <p className="text-sm text-muted-foreground">
+            Add some tags to let people know what your post is about.
+          </p>
+        </div>
+        <div className="flex flex-col gap-3 w-full">
+          <Label htmlFor="description">Description</Label>
+          <Textarea className="bg-black" placeholder="Type your message here." id="description" />
+          <p className="text-sm text-muted-foreground">
+            Introduce your thoughts by writing your description about the topic.
+          </p>
+        </div>
+        <Button>Submit</Button>
+      </div>
+    </form>
+  </div>;
 };
 
 export default WritePost;
