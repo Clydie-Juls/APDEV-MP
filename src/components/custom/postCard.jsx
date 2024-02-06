81670668d9d6295983136b5ce62b95cc501ef042
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-06 22:19:10 +0800

    Make PostCard layout more condusive to resonsiveness

--- src/components/custom/postCard.jsx
+++ src/components/custom/postCard.jsx
@@ -17,15 +17,16 @@ const PostCard = ({
     return (
         <div className='px-7 py-5 grid grid-cols-[1fr_auto] border-2 border-zinc-500 rounded-xl bg-zinc-950'>
             <div className='flex flex-col gap-2'>
-                <h3 className='text-2xl font-bold'>{title}</h3>
+                <div className='flex items-center gap-3'>
+                    <h3 className='text-2xl font-bold'>{title}</h3>
+                    {tags.map((name, i) => <Tag key={i} name={name} />)}
+                </div>
                 <p>{body}</p>
 
                 <div className='flex gap-2'>
-                    <p>By <a href="">{author}</a></p>
-                    <p>Uploaded {uploadDate}</p>
-                    <p>{views} Views</p>
-
-                    {tags.map((name, i) => <Tag key={i} name={name} />)}
+                    <p className='text-sm'>By <a href="">{author}</a></p>
+                    <p className='text-sm'>Uploaded {uploadDate}</p>
+                    <p className='text-sm'>{views} Views</p>
                 </div>
             </div>
 
