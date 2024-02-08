f12aa0b4d636efcb06c6d3dabf541cb342c7f38b
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-08 18:40:46 +0800

    Add sample api responses for posts and comments

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,6 +6,72 @@ import CardList from '@/components/custom/cardList';
 import Header from '@/components/custom/header';
 import PostCard from '@/components/custom/postCard';
 
+// TODO: Replace with actual retrieval mechanism.
+const SAMPLE_POSTS = [
+  {
+    id: 0,
+    title: 'Post 1',
+    author: 'user1',
+    body: 'body1',
+    uploadDate: '1/1/2024',
+    views: 12345,
+    likes: 123,
+    dislikes: 58,
+    userRating: 'dislike',
+    tags: ['tag1', 'tag2']
+  },
+  {
+    id: 1,
+    title: 'Post 2',
+    author: 'user2',
+    body: 'body2',
+    uploadDate: '1/1/2024',
+    views: 1245,
+    likes: 23,
+    dislikes: 8,
+    userRating: null,
+    tags: ['tag1', 'tag2']
+  },
+  {
+    id: 2,
+    title: 'Post 3',
+    author: 'user3',
+    body: 'body3',
+    uploadDate: '1/1/2024',
+    views: 1245,
+    likes: 23,
+    dislikes: 8,
+    userRating: 'like',
+    tags: ['tag1', 'tag2']
+  },
+];
+
+// TODO: Replace with actual retrieval mechanism.
+const SAMPLE_COMMENTS = [
+  {
+    id: 23,
+    postId: 1,
+    postTitle: 'Post 2',
+    postTags: ['tag3', 'tag5'],
+    body: 'Never gonna give you up. Never gonna let you down.',
+    uploadDate: '1/2/2024',
+    likes: 0,
+    dislikes: 123,
+    userRating: 'dislike'
+  },
+  {
+    id: 3,
+    postId: 0,
+    postTitle: 'Post 1',
+    postTags: ['tag3', 'tag5'],
+    body: 'We\'ve known each other, for so long',
+    uploadDate: '1/2/2024',
+    likes: 2,
+    dislikes: 0,
+    userRating: null
+  },
+];
+
 const Landing = () => {
   return (
     <AnimBackground>
