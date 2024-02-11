b1046870e8cd081ee4c307a48c86baf3fd1215ea
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-11 20:59:30 +0800

    Create TempState utility object

---
+++ src/lib/tempState.js
@@ -0,0 +1,16 @@
+if (!sessionStorage.getItem('tempState')) {
+    sessionStorage.setItem('tempState', JSON.stringify({}));
+}
+
+const actualTempState = JSON.parse(sessionStorage.getItem('tempState'));
+
+export const TempState = {
+    get(name) {
+        return actualTempState[name];
+    },
+
+    set(name, value) {
+        actualTempState[name] = value;
+        sessionStorage.setItem('tempState', JSON.stringify(actualTempState));
+    }
+};
