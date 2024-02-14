ef77343048904d7ddc11ed98520d371ff725fa3c
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-14 21:22:51 +0800

    Spice up placeholder posts in landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -7,7 +7,7 @@ import Header from '@/components/custom/header';
 import PostCard from '@/components/custom/postCard';
 
 // TODO: Replace with actual retrieval mechanism.
-const SAMPLE_POSTS = [
+const SAMPLE_POSTS_1 = [
   {
     id: 0,
     title: 'Post 1',
@@ -44,6 +44,93 @@ const SAMPLE_POSTS = [
     userRating: 'like',
     tags: ['tag1', 'tag2']
   },
+  {
+    id: 3,
+    title: 'Post 4',
+    author: 'user4',
+    body: 'body4',
+    uploadDate: '1/2/2024',
+    views: 3456,
+    likes: 87,
+    dislikes: 15,
+    userRating: 'like',
+    tags: ['tag3', 'tag4']
+  },
+  {
+    id: 4,
+    title: 'Post 5',
+    author: 'user5',
+    body: 'body5',
+    uploadDate: '1/3/2024',
+    views: 6789,
+    likes: 234,
+    dislikes: 29,
+    userRating: 'dislike',
+    tags: ['tag3', 'tag5']
+  },
+  {
+    id: 5,
+    title: 'Post 6',
+    author: 'user6',
+    body: 'body6',
+    uploadDate: '1/4/2024',
+    views: 9876,
+    likes: 345,
+    dislikes: 76,
+    userRating: 'like',
+    tags: ['tag6', 'tag7']
+  },
+];
+
+const SAMPLE_POSTS_2 = [
+  {
+    id: 6,
+    title: 'Post 7',
+    author: 'user7',
+    body: 'body7',
+    uploadDate: '1/5/2024',
+    views: 5432,
+    likes: 156,
+    dislikes: 42,
+    userRating: 'dislike',
+    tags: ['tag8', 'tag9']
+  },
+  {
+    id: 7,
+    title: 'Post 8',
+    author: 'user8',
+    body: 'body8',
+    uploadDate: '1/6/2024',
+    views: 4321,
+    likes: 67,
+    dislikes: 10,
+    userRating: 'like',
+    tags: ['tag10', 'tag11']
+  },
+  {
+    id: 8,
+    title: 'Post 9',
+    author: 'user9',
+    body: 'body9',
+    uploadDate: '1/7/2024',
+    views: 7890,
+    likes: 321,
+    dislikes: 63,
+    userRating: 'dislike',
+    tags: ['tag12', 'tag13']
+  },
+  {
+    id: 9,
+    title: 'Post 10',
+    author: 'user10',
+    body: 'body10',
+    uploadDate: '1/8/2024',
+    views: 8765,
+    likes: 432,
+    dislikes: 87,
+    userRating: 'like',
+    tags: ['tag14', 'tag15']
+  }
 ];
 
 const Landing = () => {
@@ -72,7 +159,7 @@ const Landing = () => {
               <CardList>
                 {/* TODO: Generate PostCard components based on retrived recent 
                     posts. */}
-                {SAMPLE_POSTS.map(p => 
+                {SAMPLE_POSTS_1.toSorted((a, b) => new Date(b.uploadDate) - new Date(a.uploadDate)).map(p => 
                   <PostCard
                     key={p.id}
                     title={p.title}
@@ -93,7 +180,7 @@ const Landing = () => {
             <CardList>
                 {/* TODO: Generate PostCard components based on retrived popular 
                     posts. */}
-                {SAMPLE_POSTS.map(p => 
+                {SAMPLE_POSTS_2.toSorted((a, b) => b.likes - a.likes).map(p => 
                   <PostCard
                     key={p.id}
                     title={p.title}
