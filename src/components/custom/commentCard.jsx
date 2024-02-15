13771c87fd4fab7b0d0cdf3b242764f2acaa0144
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-15 21:36:19 +0800

    Make CommentCard more clickable

--- src/components/custom/commentCard.jsx
+++ src/components/custom/commentCard.jsx
@@ -12,18 +12,21 @@ const CommentCard = ({
     userRating
 }) => {
     return (
-        <div className='px-7 py-5 overflow-hidden grid grid-cols-[1fr_auto] border-2 border-zinc-500 rounded-xl bg-zinc-950'>
-            <div className='overflow-hidden flex flex-col gap-2 justify-center'>
+        <div 
+            className='px-7 py-5 overflow-hidden grid grid-cols-[1fr_auto] border-2 border-zinc-500 rounded-xl bg-zinc-950 hover:bg-zinc-900'
+        >
+            <a 
+                className='overflow-hidden flex flex-col gap-2 justify-center'
+                href="/post"
+            >
                 <div className='flex items-center gap-3'>
-                    <a href="/post">
-                        <h3 className='text-2xl font-bold'>In {postTitle}</h3>
-                    </a>
+                    <h3 className='text-2xl font-bold'>In {postTitle}</h3>
                     <p className='text-sm'>Uploaded {uploadDate}</p>
                     {postTags.map((name, i) => <Tag key={i} name={name} />)}
                 </div>
 
                 <p className='overflow-hidden text-ellipsis text-nowrap'>{body}</p>
-            </div>
+            </a>
 
             <RateButtons likes={likes} dislikes={dislikes} userRating={userRating} />
         </div>
