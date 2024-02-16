02f00a03df774d6b6e0198da034b54d00056ea90
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-02-16 14:16:01 +0800

    comment route fix

--- src/pages/comment.jsx
+++ src/pages/comment.jsx
@@ -5,89 +5,62 @@ import { ArrowLeft } from "lucide-react";
 import { PlusSquare } from "lucide-react";
 import { useParams } from "react-router";
 import { TempComments } from "@/lib/placeholder/mockReq";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Separator } from "@/components/ui/separator";
 
 const CommentsPage = ({ isWriteComment, isReply }) => {
   const { id } = useParams();
 
   const commentToEdit = TempComments.getFromId(Number(id));
 
-  // Previous message as a draft
-  const previousMessage =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
   return (
-    <Card className="w-full h-full" style={{ border: "none" }}>
-      <CardHeader className="flex  flex-row justify-between">
-        <div style={{ flex: "0.5" }}>
-          <Button variant="ghost" onClick={() => window.history.back()}>
-            <ArrowLeft />
-          </Button>
-        </div>
-        <div style={{ flex: "2.5" }}>
-          <CardTitle className="text-4xl">
-            {isReply
-              ? "Reply"
-              : isWriteComment
-              ? "Add a Comment"
-              : "Edit Comment"}
-          </CardTitle>
-        </div>
-        <div style={{ flex: "7", justifyContent: "flex-end", display: "flex" }}>
-          <Button
-            variant="ghost"
-            style={{ border: "none", color: "#5E51FF", fontSize: "1.5rem" }}
-          >
-            Post
-          </Button>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4 mb-4">
-            <div className="flex flex-col gap-3">
-              <hr
-                style={{
-                  width: "100%",
-                  height: "2px",
-                  backgroundColor: "white",
-                  border: "none",
-                }}
-              />
-              <textarea
-                id="comment"
-                placeholder="Write your comment here"
-                rows={29}
-                defaultValue={
-                  isReply ? "" : isWriteComment ? "" : previousMessage
-                }
-                value={isWriteComment ? "" : commentToEdit.body}
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  border: "1px solid white",
-                  borderRadius: "10px",
-                  padding: "15px",
-                  marginTop: "40px",
-                  marginLeft: "40px",
-                  marginRight: "40px",
-                }}
-              />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <hr
-        style={{
-          width: "100%",
-          height: "2px",
-          backgroundColor: "gray",
-          border: "none",
-        }}
+    <div className="w-screen h-screen px-6 py-10 flex flex-col gap-6 items-center overflow-x-hidden">
+      <img
+        className=" w-full h-full object-cover fixed -z-10 brightness-[0.15]"
+        src="/images/star-bg.png"
+        alt="cube background image"
       />
-      <Button variant="ghost" style={{ border: "none", padding: "3rem" }}>
-        <PlusSquare style={{ width: "3rem", height: "3rem" }} />
-      </Button>
-    </Card>
+      <div className="flex flex-col gap-4 w-full sticky top-0 bg-black">
+        <h1 className=" text-4xl font-bold">
+          {isWriteComment ? "Write your Comment" : "Edit your comment"}
+        </h1>
+        <p className="text-muted-foreground">
+          Share your tech-tacular post that peaks the community`s interest
+        </p>
+        <Separator />
+      </div>
+      <form className="w-full">
+        <div className="flex flex-col gap-8 items-center px-[10%] w-full ">
+          <div className="flex flex-col gap-3 w-full py-8">
+            <Label htmlFor="description" className=" text-2xl">
+              Comment
+            </Label>
+            {isWriteComment ? (
+              <Textarea
+                className="bg-black"
+                placeholder="Type your message here."
+                id="description"
+              />
+            ) : (
+              <Textarea
+                className="bg-black min-h-[500px]"
+                id="description"
+                value={commentToEdit.body}
+              />
+            )}
+
+            <p className="text-sm text-muted-foreground">
+              Share your insights or feedback by writing a comment on the topic.
+            </p>
+          </div>
+
+          <Button className="px-9">
+            <a href="/post">Submit</a>
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
