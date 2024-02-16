cc23fbcd5c62992d07b8041c8ec6ada1db29dfef
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-02-16 10:37:04 +0800

    fix routing issues

--- src/components/custom/commentBody.jsx
+++ src/components/custom/commentBody.jsx
@@ -1,90 +1,105 @@
-import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Send, MoreVertical } from 'lucide-react';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import RateButtons from '@/components/custom/rateButtons';
-import { Link } from 'react-router-dom';
+import { Send, MoreVertical } from "lucide-react";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import RateButtons from "@/components/custom/rateButtons";
+import { Link } from "react-router-dom";
 
-const CommentBody = ({ profile, nestedProfile, userName, nestedUserName, paragraph, nestedParagraph, isReply, isOwner }) => {
-    return (
-      <Card className="mb-2"> 
-        <CardHeader className="flex flex-row">
-          <a href="/user" className='flex items-center gap-2'>
-            <Avatar>
-              <AvatarImage src={profile} />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            {userName}
-          </a>
-        </CardHeader>
-  
-        <CardContent>
-          {isReply ? (
-            <Card className="p-4">
-              <CardHeader className="p-0 space-y-0 flex flex-row">
-                <div className='flex items-center gap-4'>
-                  Replying to
-                  <a href="/user" className='flex items-center gap-2'>
-                    <Avatar>
-                      <AvatarImage src={nestedProfile} />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    {nestedUserName}
-                  </a>
-                </div>
-              </CardHeader>
-              <CardContent className="p-0 mt-2 text-zinc-400 text-ellipsis text-nowrap overflow-hidden">
-                {nestedParagraph}
-              </CardContent>
-            </Card>
-          ) : null}
-  
-          {/* Reply */}
-          <div className="mt-6 grid w-full items-center">
-            <div className="flex flex-col">
-              {paragraph}
-            </div>
-          </div>
-        </CardContent>
-  
-        <CardFooter className="flex">
-          <RateButtons likes={50} dislikes={10} horizontal />
-          {isOwner ? (
-            <>
-              <div>
-                <Button variant="ghost" style={{ width: '100px' }} onClick={() => window.location.replace('/reply')}>
-                  <Send style={{ width: '1.5rem', height: '1.5rem' }} />
-                  <a href="/reply">Reply</a>
-                </Button>
+const CommentBody = ({
+  posterId,
+  profile,
+  nestedProfile,
+  userName,
+  nestedUserName,
+  paragraph,
+  nestedParagraph,
+  isReply,
+  isOwner,
+}) => {
+  return (
+    <Card className="mb-2">
+      <CardHeader className="flex flex-row">
+        <a href={`/user/${posterId}`} className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src={profile} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {userName}
+        </a>
+      </CardHeader>
+
+      <CardContent>
+        {isReply ? (
+          <Card className="p-4">
+            <CardHeader className="p-0 space-y-0 flex flex-row">
+              <div className="flex items-center gap-4">
+                Replying to
+                <a href="/user" className="flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src={nestedProfile} />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  {nestedUserName}
+                </a>
               </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger variant="ghost" className="ml-[15px]">
-                  <MoreVertical style={{ width: '1.5rem', height: '1.5rem' }} />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem
-                    onSelect={() => window.location.replace('/editcomment')}
-                  >
-                    Edit comment
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    onClick={() => setConfirmDelete(true)}
-                  >
-                    Delete comment
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </>
-          ) : null}
-        </CardFooter>
-      </Card>
-    );
-  };
-  
-  export default CommentBody;
+            </CardHeader>
+            <CardContent className="p-0 mt-2 text-zinc-400 text-ellipsis text-nowrap overflow-hidden">
+              {nestedParagraph}
+            </CardContent>
+          </Card>
+        ) : null}
+
+        {/* Reply */}
+        <div className="mt-6 grid w-full items-center">
+          <div className="flex flex-col">{paragraph}</div>
+        </div>
+      </CardContent>
+
+      <CardFooter className="flex">
+        <RateButtons likes={50} dislikes={10} horizontal />
+        {isOwner ? (
+          <>
+            <div>
+              <Button
+                variant="ghost"
+                style={{ width: "100px" }}
+                onClick={() => window.location.replace("/reply")}
+              >
+                <Send style={{ width: "1.5rem", height: "1.5rem" }} />
+                <a href="/reply">Reply</a>
+              </Button>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger variant="ghost" className="ml-[15px]">
+                <MoreVertical style={{ width: "1.5rem", height: "1.5rem" }} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  onSelect={() => window.location.replace("/editcomment")}
+                >
+                  Edit comment
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setConfirmDelete(true)}>
+                  Delete comment
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : null}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CommentBody;
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,13 +1,13 @@
-import { Button } from '@/components/ui/button';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import AnimBackground from '@/components/custom/animBackground';
-import CardList from '@/components/custom/cardList';
-import Header from '@/components/custom/header';
-import PostCard from '@/components/custom/postCard';
+import AnimBackground from "@/components/custom/animBackground";
+import CardList from "@/components/custom/cardList";
+import Header from "@/components/custom/header";
+import PostCard from "@/components/custom/postCard";
 
-import { TempUsers } from '@/lib/placeholder/mockReq';
-import { Posts } from '@/lib/placeholder/posts';
+import { TempUsers } from "@/lib/placeholder/mockReq";
+import { Posts } from "@/lib/placeholder/posts";
 
 const Landing = () => {
   return (
@@ -23,19 +23,22 @@ const Landing = () => {
                   <TabsTrigger value="recent">Recent</TabsTrigger>
                   <TabsTrigger value="popular">Popular</TabsTrigger>
                 </TabsList>
-                <h2 className='text-3xl font-bold'>Posts</h2>
+                <h2 className="text-3xl font-bold">Posts</h2>
               </div>
 
               <Button asChild>
                 <a href="/writePost">Create a Post</a>
               </Button>
             </div>
-        
+
             <TabsContent value="recent">
               <CardList>
-                {Posts.toSorted((a, b) => new Date(b.uploadDate) - new Date(a.uploadDate)).map(p => 
+                {Posts.toSorted(
+                  (a, b) => new Date(b.uploadDate) - new Date(a.uploadDate)
+                ).map((p) => (
                   <PostCard
                     key={p.id}
+                    id={p.id}
                     title={p.title}
                     author={TempUsers.getInfoFromId(p.posterId).name}
                     body={p.body}
@@ -43,16 +46,18 @@ const Landing = () => {
                     views={p.views}
                     likes={p.likerIds.length}
                     dislikes={p.dislikerIds.length}
-                    userRating={p.likerIds.includes(0) ? 'like' : 'dislike'}
+                    userRating={p.likerIds.includes(0) ? "like" : "dislike"}
                     tags={p.tags}
                   />
-                )}
+                ))}
               </CardList>
             </TabsContent>
 
             <TabsContent value="popular">
               <CardList>
-                {Posts.toSorted((a, b) => b.likerIds.length - a.likerIds.length).map(p => 
+                {Posts.toSorted(
+                  (a, b) => b.likerIds.length - a.likerIds.length
+                ).map((p) => (
                   <PostCard
                     key={p.id}
                     title={p.title}
@@ -62,10 +67,10 @@ const Landing = () => {
                     views={p.views}
                     likes={p.likerIds.length}
                     dislikes={p.dislikerIds.length}
-                    userRating={p.likerIds.includes(0) ? 'like' : 'dislike'}
+                    userRating={p.likerIds.includes(0) ? "like" : "dislike"}
                     tags={p.tags}
                   />
-                )}
+                ))}
               </CardList>
             </TabsContent>
           </Tabs>
