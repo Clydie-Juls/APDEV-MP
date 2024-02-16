2c1189a3ec1ee7975f180fc3990e57de97b90305
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-02-16 13:57:04 +0800

    fix 2

--- src/components/custom/animBackground.jsx
+++ src/components/custom/animBackground.jsx
@@ -1,6 +1,6 @@
 const AnimBackground = ({ className, children }) => {
   return (
-    <div className={className + " relative min-h-screen grid"}>
+    <div className={className + " relative min-h-screen flex"}>
       <img
         className=" w-full h-full object-cover absolute -z-10 brightness-[0.1]"
         src="/images/animBackground.gif"
