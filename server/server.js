8a016cd5592a45b3b42700b7976cc1df7098f9c3
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-02-27 13:34:52 +0800

    implement auth middleware to request routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { User } from "../models/user.js";
 import { Post } from "../models/post.js";
 import { Comment } from "../models/comment.js";
+import { isAuth, username } from "middleware/auth.js";
 
 const app = express();
 const port = 3000;
@@ -14,7 +15,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/T3Db");
 // middleware setup
 app.use(express.urlencoded({ extended: true }));
 // GET HTTP requests
-apiRouter.get("/users/:id", (req, res) => {
+apiRouter.get("/users/:id", isAuth, (req, res) => {
   try {
     const { id } = req.params;
     // user db fetch
@@ -30,7 +31,7 @@ apiRouter.get("/users/:id", (req, res) => {
   }
 });
 
-apiRouter.get("/posts/:id", (req, res) => {
+apiRouter.get("/posts/:id", isAuth, (req, res) => {
   try {
     const { id } = req.params;
     // post db fetch
@@ -48,14 +49,14 @@ apiRouter.get("/posts/:id", (req, res) => {
 
 // /search?q=magic&t=a,b,c&do=asc&po=asc
 apiRouter.get("/search", async (req, res) => {
-  const query = req.query.q || '';
+  const query = req.query.q || "";
   const tags = req.query.t || [];
-  
-  const dateOrder = req.query.do || 'asc';
-  const popularityOrder = req.query.po || 'asc';
-  
+
+  const dateOrder = req.query.do || "asc";
+  const popularityOrder = req.query.po || "asc";
+
   const posts = await Post.find({
-    'title': { '$regex': query, '$options': 'i' }
+    title: { $regex: query, $options: "i" },
   });
 
   // TODO
@@ -68,22 +69,22 @@ apiRouter.post("/login", (req, res) => {
 });
 
 apiRouter.post("/signup", (req, res) => {
-  User.create({ 
+  User.create({
     username: req.body.username,
     password: req.body.password,
-    description: '',
-    picture: null
+    description: "",
+    picture: null,
   });
 
   // TODO: Auto log-in the user.
-  res.status(201).redirect('/');
+  res.status(201).redirect("/");
 });
 
 apiRouter.post("/editlogininfo", (req, res) => {
   res.status(201).send("edit login info successful");
 });
 
-apiRouter.post("/writepost", async (req, res) => {
+apiRouter.post("/writepost", isAuth, async (req, res) => {
   // TODO
   const poster = await User.findOne();
 
@@ -94,24 +95,24 @@ apiRouter.post("/writepost", async (req, res) => {
     comments: [],
     reactions: {
       likerIds: [],
-      dislikerIds: []
+      dislikerIds: [],
     },
-    tags: []
+    tags: [],
   });
-  
+
   // TODO: Redirect user to the page of their post
-  res.status(201).redirect('/');
+  res.status(201).redirect("/");
 });
 
-apiRouter.post("/editposts/:id", (req, res) => {
+apiRouter.post("/editposts/:id", isAuth, (req, res) => {
   res.status(201).send("edit post successful");
 });
 
-apiRouter.post("/writecomment", (req, res) => {
+apiRouter.post("/writecomment", isAuth, (req, res) => {
   res.status(201).send("write comment successful");
 });
 
-apiRouter.post("/editcomments/:id", (req, res) => {
+apiRouter.post("/editcomments/:id", isAuth, (req, res) => {
   res.status(201).send("edit comment successful");
 });
 
